0ee9fb0612ad39b8635f4a5edd3da19494feac89
SanuCoder <91316341+SanuCoder@users.noreply.github.com>
2021-09-25 18:10:51 +0530

    Update funcn.py

--- helper/funcn.py
+++ helper/funcn.py
@@ -18,7 +18,7 @@ COUNT = []
 OK = {}
 
 uptime = dt.now()
-os.system("wget https://telegra.ph/file/75ee20ec8d8c8bba84f02.jpg -O thumb.jpg")
+os.system("wget https://telegra.ph/file/5f26617f6335ad165f519.jpg -O thumb.jpg")
 
 if not os.path.isdir("downloads/"):
     os.mkdir("downloads/")
